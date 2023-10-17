3e5ca74801635f2716eb0ac06cb879b986f93eb7
leobewater <mmbee888@gmail.com>
2023-10-17 18:01:41 -0400

    add heroes

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
 const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
@@ -11,6 +14,10 @@ const Heading = () => {
         Jotion is the connected workspace where <br />
         better, faster work happens.
       </h3>
+      <Button>
+        Enter Jotion
+        <ArrowRight className="h-4 w-4 ml-2" />
+      </Button>
     </div>
   );
 };
