aaa52fb6545f442eea8b3d9f4d7ab38da9758e6d
leobewater <mmbee888@gmail.com>
2023-10-17 18:18:26 -0400

    make navbar sticky

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import React from "react";
+import Logo from "./logo";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const scrolled = useScrollTop();
 
-export default Navbar
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+    </div>
+  );
+};
+
+export default Navbar;
