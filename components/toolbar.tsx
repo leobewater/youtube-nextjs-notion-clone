cf24cc09deffbab25a6f4c883655d928312bc47f
leobewater <mmbee888@gmail.com>
2023-10-18 12:21:29 -0400

    add close button to icon picker

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -2,6 +2,8 @@
 
 import { Doc } from "@/convex/_generated/dataModel";
 import { IconPicker } from "./icon-picker";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -18,6 +20,14 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
               {initialData.icon}
             </p>
           </IconPicker>
+          <Button
+            onClick={() => {}}
+            className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
+            variant="outline"
+            size="icon"
+          >
+            <X className="h-4 w-4" />
+          </Button>
         </div>
       )}
       Toolbar
