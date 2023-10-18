74a9958158fe59171af83a58749f76fe36017069
leobewater <mmbee888@gmail.com>
2023-10-18 05:29:27 -0400

    setup navigation sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,19 @@
 "use client";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <>
+      <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
+        <div>
+          <p>Action items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover:opacity-100" />
+      </aside>
+    </>
+  );
 };
 
 export default Navigation;
