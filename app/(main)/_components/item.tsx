7effd4ae049350787b32365f8f2e67c8fd096268
leobewater <mmbee888@gmail.com>
2023-10-18 08:14:51 -0400

    add expand note logic

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -31,6 +31,14 @@ const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+    // call the optional onExpand function
+    onExpand?.();
+  };
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
   return (
@@ -49,7 +57,7 @@ const Item = ({
         <div
           role="button"
           className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-          onClick={() => {}}
+          onClick={handleExpand}
         >
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground" />
         </div>
