59361deb1bb73c67ce42f5c88890b94372f11d5a
leobewater <mmbee888@gmail.com>
2023-10-18 12:11:18 -0400

    set up doc toolbar component

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,7 +1,17 @@
-'use client'
+"use client";
 
-export const Toolbar = () => {
-  return (
-    <div>Toolbar</div>
-  )
+import { Doc } from "@/convex/_generated/dataModel";
+
+interface ToolbarProps {
+  initialData: Doc<"documents">;
+  preview?: boolean;
 }
+
+export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  return <div className="pl-[54px] group relative">
+    {!!initialData.icon && !preview && (
+      <div className="flex items-center gap-x-2 group/icon pt-6">
+      </div>
+    )}
+    Toolbar</div>;
+};
