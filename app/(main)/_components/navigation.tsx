9246e5ac7ecaefd49d251290264418ba20f4cbde
leobewater <mmbee888@gmail.com>
2023-10-18 14:55:26 -0400

    redirect to doc detail page after added

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -9,7 +9,7 @@ import {
   Settings,
   Trash,
 } from "lucide-react";
-import { useParams, usePathname } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { useMutation } from "convex/react";
@@ -29,6 +29,7 @@ import { useSettings } from "@/hooks/use-settings";
 import { Navbar } from "./navbar";
 
 const Navigation = () => {
+  const router = useRouter();
   const settings = useSettings();
   const search = useSearch();
 
@@ -126,7 +127,9 @@ const Navigation = () => {
   };
 
   const handleCreate = () => {
-    const promise = create({ title: "Untitled" });
+    const promise = create({ title: "Untitled" }).then((documentId) => {
+      router.push(`/documents/${documentId}`);
+    });
 
     toast.promise(promise, {
       loading: "Creating a new note...",
