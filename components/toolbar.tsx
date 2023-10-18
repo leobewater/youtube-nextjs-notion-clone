2e4fda477581ef870eee24299709057f96a5f0be
leobewater <mmbee888@gmail.com>
2023-10-18 12:32:25 -0400

    npm i react-textarea-autosize

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { ElementRef, useRef, useState } from "react";
 import { Doc } from "@/convex/_generated/dataModel";
+import { ImageIcon, Smile, X } from "lucide-react";
 import { IconPicker } from "./icon-picker";
 import { Button } from "./ui/button";
-import { Smile, X } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -11,6 +14,41 @@ interface ToolbarProps {
 }
 
 export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update);
+
+  const enableInput = () => {
+    if (preview) return;
+
+    setIsEditing(true);
+    setTimeout(() => {
+      setValue(initialData.title);
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const disableInput = () => {
+    setIsEditing(false);
+  };
+
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+  };
+
   return (
     <div className="pl-[54px] group relative">
       {!initialData.icon && !preview && (
@@ -47,7 +85,21 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
             </Button>
           </IconPicker>
         )}
+        {!initialData.coverImage && !preview && (
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Add cover
+          </Button>
+        )}
       </div>
+      {isEditing && !preview ? (
+
+      ) : ()}
     </div>
   );
 };
